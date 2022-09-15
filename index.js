dd684115ef3d7a0ea36c2405d6939056f36829e5
cheng <y2984491@gmail.com>
2022-09-15 13:47:53 -0400

    finished
    spent an hour do code this game while following freecodecamp yt tutorial

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("javascript page added")
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let result;
+let userChoice;
+let computerChoice;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener(
+    "click",(e) =>{
+        userChoice = e.target.id
+        userChoiceDisplay.innerHTML = userChoice;
+        generateComputerChoice();
+        getResults();
+    }
+))
+
+function generateComputerChoice(){
+    let random = Math.floor(Math.random() * possibleChoices.length + 1)
+    if(random == 1){
+        computerChoice = 'rock'
+    } else if(random == 2){
+        computerChoice = "scissors"
+    } else {
+        computerChoice = "paper"
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResults (){
+    if(computerChoice == userChoice){
+        result = "its a draw!"
+    } else if (computerChoice == "rock" && userChoice == "paper"){
+        result = "you win!"
+    } else if(computerChoice =="rock" && userChoice == "scissors"){
+        result = "you lose!"
+    } else if(computerChoice == "paper" && userChoice =="rock"){
+        result = "you lose!"
+    } else if(computerChoice == "paper" && userChoice == "scissors"){
+        result = "you win!"
+    } else if(computerChoice =="scissors" && userChoice =="rock"){
+        result = "you win!" 
+    } else if(computerChoice =="scissors" && userChoice =="paper"){
+        result = "you lose!"
+    }
+    resultDisplay.innerHTML = result;
+}
